c69ce8707bef12261dc2d052395681ddd4202130
Biruk Kebede <biruk.key@gmail.com>
2021-06-13 01:14:59 +0100

    Material ui admin login component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,9 +3,12 @@ import { Link } from "react-router-dom";
 
 import "./Home.css";
 import logo from "./logo.svg";
+import AdminLogIn from './AdminLogIn';
+import { Button } from '@material-ui/core';
 
 export function Home() {
 	const [message, setMessage] = useState("Loading...");
+	const [logIn, setLogIn] = useState(false);
 
 	useEffect(() => {
 		fetch("/api")
@@ -26,9 +29,15 @@ export function Home() {
 	return (
 		<main role="main">
 			<div>
-				<img className="logo" data-qa="logo" src={logo} alt="Just the React logo" />
+				<header className='header'>
+					<img className="logo" data-qa="logo" src={logo} alt="Just the React logo" />
+					<div className='about-and-login'>
+						<Link className='about' to="/about/this/site">About</Link>
+						<Button variant='outlined' color='primary' onClick={() => setLogIn(true)}>Admin Log In</Button>
+					</div>
+				</header>
 				<h1 className="message" data-qa="message">{message}</h1>
-				<Link to="/about/this/site">About</Link>
+				<AdminLogIn logIn={logIn} setLogIn={setLogIn}/>
 			</div>
 		</main>
 	);
