b89de050a4211eab0df0924a286b4b7484885cd3
Cecilia <earwen1986@gmail.com>
2021-06-18 14:40:47 +0100

    Restyled admin page

--- client/src/components/DonateLink.js
+++ client/src/components/DonateLink.js
@@ -3,7 +3,11 @@ import "./DonateLink.css"
 const DonateLink = () => {
 	return (
     <div className="donate">
-      <a href="https://www.migranthelpuk.org/donate/childrensfund/5/credit-card" target="_blank">
+      <a
+        href="https://www.migranthelpuk.org/donate/childrensfund/5/credit-card"
+        target="_blank"
+        rel="noreferrer"
+      >
         DONATE
       </a>
     </div>
