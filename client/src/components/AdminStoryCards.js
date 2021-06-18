2ab181bdaea5133642bca0c6c8af5d8b560cef5a
Cecilia <earwen1986@gmail.com>
2021-06-18 11:39:59 +0100

    Changed approval/rejection message

--- client/src/components/AdminStoryCards.js
+++ client/src/components/AdminStoryCards.js
@@ -50,7 +50,7 @@ const AdminStoryCards = ({user}) => {
       })
       .then((data) => {
         if (data.success) {
-          alert(data.success);
+          alert(`Item successfully ${newStatus}`);
           fetch("/api/artwork?status=submitted")
             .then((res) => res.json())
             .then((data) => setSubmittedArtwork(data))
