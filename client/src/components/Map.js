7240bbe41c555e96f9ce8b970db883fb6ed71ed3
Cecilia <earwen1986@gmail.com>
2021-06-22 09:47:48 +0100

    Changed map background to black/white

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -18,7 +18,7 @@ const Map = ({ approvedArtwork }) => {
     >
       <TileLayer
         attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}{r}.png"
+        url="https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.png"
       />
       {filteredArtwork.map((el) => (
         <CircleMarker
