34cfd7d110eb4d83b6fead30d7d66dbd28c49525
Cecilia <earwen1986@gmail.com>
2021-06-22 15:19:58 +0100

    Improved appearance of admin creation form

--- client/src/components/NewAdmin.js
+++ client/src/components/NewAdmin.js
@@ -1,8 +1,27 @@
 import { useState, useEffect } from "react";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  form: {
+    width: "50%",
+    minWidth: "300px",
+    marginTop: theme.spacing(1),
+    backgroundColor: "white",
+	borderRadius: "5px",
+	padding: "1rem"
+  },
+  formContainer: {
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+}));
 
 const NewAdmin = ({ setCreateMode }) => {
+  const classes = useStyles();
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
 
@@ -24,40 +43,42 @@ const NewAdmin = ({ setCreateMode }) => {
     });
   };
   return (
-    <form>
-      <TextField
-        variant="outlined"
-        margin="normal"
-        required
-        fullWidth
-        id="username"
-        label="User name"
-        name="username"
-        autoFocus
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <TextField
-        variant="outlined"
-        margin="normal"
-        required
-        fullWidth
-        id="email"
-        label="Email"
-        name="email"
-        autoComplete="email"
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <Button
-        type="submit"
-        id="submit"
-        fullWidth
-        variant="contained"
-        color="primary"
-        onClick={handleSubmit}
-      >
-        Create new admin account
-      </Button>
-    </form>
+    <div className={classes.formContainer}>
+      <form className={classes.form}>
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          id="username"
+          label="User name"
+          name="username"
+          autoFocus
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          id="email"
+          label="Email"
+          name="email"
+          autoComplete="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Button
+          type="submit"
+          id="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          onClick={handleSubmit}
+        >
+          Create new admin account
+        </Button>
+      </form>
+    </div>
   );
 };
 
