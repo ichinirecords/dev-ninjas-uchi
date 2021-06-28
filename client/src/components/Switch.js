8e48c570934fdc50804a7d547f9a96722a7bb3ee
Biruk Kebede <biruk.key@gmail.com>
2021-06-28 14:23:13 +0100

    Media types checkbox switch togglers are now functional

--- client/src/components/Switch.js
+++ client/src/components/Switch.js
@@ -1,82 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Switch.css';
 
-const Switch = () => {
+const Switch = ({ approvedArtwork, setApprovedArtwork, backupData }) => {
+  let tempArray = [...approvedArtwork]
+  const mediaTypes = ['video', 'audio', 'image', 'text'];
+  const [checkedMediaTypes, setCheckedMediaTypes] = useState([]);
+  const [checkedState, setCheckedState] = useState(
+    new Array(mediaTypes.length).fill(true)
+  );
+  
+  const handleCheckbox = (e, position) => {
+    const updatedCheckedState = checkedState.map((checked, index) =>
+      index === position ? !checked : checked
+    );
+    setCheckedState(updatedCheckedState);
+    const flatDeep = (arr, d = 1) => {
+      return d > 0 ? arr.reduce((acc, val) => acc.concat(Array.isArray(val) ? flatDeep(val, d - 1) : val), [])
+        : arr.slice();
+    };
+
+    if (!e.target.checked) {
+      const unchecked = tempArray.filter(artWork => artWork.content_type !== mediaTypes[position]);
+      const flattened = flatDeep(unchecked, Infinity);
+      setApprovedArtwork(flattened);
+      setCheckedMediaTypes(flattened);
+    } else if (e.target.checked) {
+      let array = [...backupData]
+      const checked = array.filter(artWork => artWork.content_type === mediaTypes[position]);
+      tempArray = [...checkedMediaTypes, ...checked]
+      const flattened = flatDeep(tempArray, Infinity);
+      setApprovedArtwork(flattened);
+      setCheckedMediaTypes(flattened);
+    }
+  }
+
   return (
     <div className='radio-switch'>
-      <div className='video'>
-        <div>
-          <input
-            className="switch-checkbox"
-            id={`video`}
-            type="checkbox"
-          />
-          <label
-            className="switch-label"
-            htmlFor={`video`}
-          >
-            <span className={`switch-button`} />
-          </label>
-        </div>
-        <div>
-          <h2>Video</h2>
-        </div>
-
-      </div>
-      <div className='music'>
-        <div>
-          <input
-            className="switch-checkbox"
-            id={`image`}
-            type="checkbox"
-          />
-          <label
-            className="switch-label"
-            htmlFor={`image`}
-          >
-            <span className={`switch-button`} />
-          </label>
-        </div>
-        <div>
-          <h2>Music</h2>
-        </div>
-      </div>
-      <div className='image'>
-        <div>
-          <input
-            className="switch-checkbox"
-            id={`music`}
-            type="checkbox"
-          />
-          <label
-            className="switch-label"
-            htmlFor={`music`}
-          >
-            <span className={`switch-button`} />
-          </label>
-        </div>
-        <div>
-          <h2>Image</h2>
-        </div>
-      </div>
-      <div className='text'>
-        <div>
-          <input
-            className="switch-checkbox"
-            id={`text`}
-            type="checkbox"
-          />
-          <label
-            className="switch-label"
-            htmlFor={`text`}
-          >
-            <span className={`switch-button`} />
-          </label>
-        </div>
-        <div>
-          <h2>Text</h2>
-        </div>
-      </div>
+      {mediaTypes.map((mediaType, index) => {
+        return (
+          <div key={index} className={mediaType}>
+            <div>
+              <input
+                className="switch-checkbox"
+                id={index}
+                name={mediaType}
+                value={mediaType}
+                type="checkbox"
+                checked={checkedState[index]}
+                onChange={(e) => {
+                  handleCheckbox(e, index)
+                }}
+              />
+              <label
+                className="switch-label"
+                htmlFor={index}
+              >
+                <span className={`switch-button`} />
+              </label>
+            </div>
+            <div>
+              <h2>{mediaType}</h2>
+            </div>
+          </div>
+        )
+      })}
     </div>
   );
 };
